6536d7f5ff29b707cc3fe8bc24fc056b07f234ca
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2024-10-10 17:37:22 -0400

    Add packet macros!

--- src/common/Packets.cs
+++ src/common/Packets.cs
@@ -1,7 +1,14 @@
 using LiteNetLib;
 using LiteNetLib.Utils;
 
-static class Packets
+namespace Common;
+
+public interface IPacket : INetSerializable
+{
+    int PacketCode();
+}
+
+static partial class Packets
 {
     private static readonly NetDataWriter writer = new();
 
@@ -12,24 +19,14 @@ static class Packets
     /// <summary>The local tick value.</summary>
     public static int LocalTick;
 
-    public static void Send<T>(this NetPeer peer, T value, DeliveryMethod deliveryMethod = DeliveryMethod.ReliableOrdered) where T : IPacket
+    public static void Send<T>(this NetPeer peer, T value, DeliveryMethod deliveryMethod = DeliveryMethod.ReliableOrdered)
+        where T : IPacket
     {
         writer.Reset();
-        writer.Put(LocalTick);
         writer.Put((byte)value.PacketCode());
+        writer.Put(LocalTick);
         writer.Put(value);
         peer.Send(writer, deliveryMethod);
-    }
-
-    public static void Receive(NetPeer _peer, NetPacketReader reader, byte _channel, DeliveryMethod _deliveryMethod)
-    {
-        int senderTick = reader.GetInt();
-        byte type = reader.GetByte();
-        switch (type) {
-            case 0x10: IntroduceClient.Queue.Enqueue(senderTick, reader.Get<IntroduceClient>()); break;
-            case 0x11: SyncTick.Queue.Enqueue(senderTick, reader.Get<SyncTick>()); break;
-        }
-        AssumeEqual(reader.AvailableBytes, 0, twoExpr: "expected");
     }
 }
 
@@ -72,44 +69,4 @@ sealed class PacketQueue<T> where T : struct
         (senderTick, packet) = packets.Dequeue();
         return true;
     }
-}
-
-interface IPacket : INetSerializable
-{
-    int PacketCode();
-}
-
-#pragma warning disable IDE0250
-#pragma warning disable IDE0251 // Make member 'readonly'
-
-record struct IntroduceClient(int PlayerID, string SlugcatWorld, string StartingRoom) : IPacket
-{
-    public static readonly PacketQueue<IntroduceClient> Queue = new();
-
-    public int PacketCode() => 0x10;
-
-    public void Deserialize(NetDataReader reader)
-    {
-        PlayerID = reader.GetInt();
-        SlugcatWorld = reader.GetString();
-        StartingRoom = reader.GetString();
-    }
-
-    public void Serialize(NetDataWriter writer)
-    {
-        writer.Put(PlayerID);
-        writer.Put(SlugcatWorld);
-        writer.Put(StartingRoom);
-    }
-}
-
-record struct SyncTick() : IPacket
-{
-    public static readonly PacketQueue<SyncTick> Queue = new();
-
-    public int PacketCode() => 0x11;
-
-    public void Deserialize(NetDataReader reader) { }
-
-    public void Serialize(NetDataWriter writer) { }
 }
---
+++ src/sourcegen/Program.cs
@@ -0,0 +1,100 @@
+﻿string packets = """
+    0x10 AddClient
+        i32 PlayerID
+        str SlugcatWorld
+        str StartingRoom
+    0x11 SyncTick
+    """;
+
+string generated = """
+    using LiteNetLib;
+    using LiteNetLib.Utils;
+    
+    namespace Common;
+    
+    """;
+
+string SplitWord()
+{
+    string[] split = packets.Split([' ', '\n'], 2);
+    packets = split.Length == 1 ? "" : split[1].Trim();
+    return split[0].Trim();
+}
+
+static string ToCSharp(string ty) => ty switch
+{
+    "i32" => "int",
+    "str" => "string",
+    _ => "ERROR",
+};
+
+List<(string n, string c)> packetNames = [];
+
+while (packets.Length > 0) {
+    string packetCode = SplitWord();
+    string packetName = SplitWord();
+    List<(string ty, string name)> fields = [];
+
+    while (!packets.StartsWith('0') && packets.Length > 0) {
+        fields.Add((ToCSharp(SplitWord()), SplitWord()));
+    }
+
+    string fieldString = string.Join(", ", fields.Select(t => $"{t.ty} {t.name}"));
+    string fieldDeserialize = string.Join("\n        ", fields.Select(t => $"{t.name} = reader.Get{char.ToUpper(t.ty[0]) + t.ty[1..]}();"));
+    string fieldSerialize = string.Join("\n        ", fields.Select(t => $"writer.Put({t.name});"));
+
+    generated += $$"""
+
+        record struct {{packetName}}({{fieldString}}) : IPacket
+        {
+            public static readonly PacketQueue<{{packetName}}> Queue = new();
+
+            public int PacketCode() => {{packetCode}};
+
+            public void Deserialize(NetDataReader reader)
+            {
+                {{fieldDeserialize}}
+            }
+
+            public void Serialize(NetDataWriter writer)
+            {
+                {{fieldSerialize}}
+            }
+        }
+
+        """;
+
+    packetNames.Add((packetName, packetCode));
+}
+
+string cases = string.Join(
+    "\n            ", 
+    packetNames.Select(t => $"case {t.c}: {t.n}.Queue.Enqueue(senderTick, reader.Get<{t.n}>()); break;")
+    );
+
+generated += $$"""
+
+    static partial class Packets
+    {
+        public static void Receive(NetPeer _peer, NetPacketReader reader, byte _channel, DeliveryMethod _deliveryMethod)
+        {
+            byte type = reader.GetByte();
+            int senderTick = reader.GetInt();
+            switch (type) {
+                {{cases}}
+            }
+            AssumeEqual(reader.AvailableBytes, 0, twoExpr: "expected");
+        }
+    }
+
+    """;
+
+// Normalize line endings.
+generated = generated.Replace("\r\n", "\n").Replace("\n", "\r\n");
+
+// Write it all to the src/common folder.
+string path = Environment.ProcessPath;
+path = path[..path.LastIndexOf("\\src\\")];
+path += "/src/common/Packets.generated.cs";
+
+File.WriteAllText(path, generated);
